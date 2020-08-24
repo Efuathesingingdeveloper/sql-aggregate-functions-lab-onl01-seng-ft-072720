8542405726e85046160948559199f4a446c20865
Georgette Class-Peters <georgycp@udel.edu>
2020-08-24 20:48:01 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,8 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  
+  SELECT MAX(lowest_student_gpa) FROM gpa;
+
 end
 
 def lowest_student_gpa
